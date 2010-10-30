ac13d474d6fd827377ae3db78dbd45d70887f951
Luís Brandão <luisbrandao@gmail.com>
2010-10-30 19:41:12 +0200

    first version

--- weight_histogram.m
+++
@@ -1,8 +0,0 @@
-% Project: Mean-shift tracker
-% Author: Luís Brandão
-% University of Amsterdam, Intelligent Multimedia Systems course - Fall 2009
-
-function [weighted_histogram] = weight_histogram(histogram, kernel_mask)
-    weighted_histogram = histogram .* kernel_mask
-end
-
